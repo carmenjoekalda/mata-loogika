240a3fa86f5bf6bcf0b7639587b013f204da6d07
cjoekalda <cjoekalda@gmail.com>
2022-10-19 10:03:04 +0300

    Math expressions

---
+++ math_expressions.py
@@ -0,0 +1,76 @@
+import math
+
+'''Practice different math expressions and calculations.'''
+
+# Declare num_a and num_b
+
+num_a = 9
+num_b = 4
+
+# 1. Sum and difference
+
+sum = num_a + num_b
+print(f"{num_a} + {num_b} = {sum}")
+
+difference = num_a - num_b
+print(f"{num_a} - {num_b} = {difference}")
+
+print("--------------------------------------")
+
+# 2. Float division
+
+division = num_a / num_b
+print(f"{num_a} / {num_b} = {division}")
+
+print("--------------------------------------")
+
+# 3. Integer division
+
+division = num_a // num_b
+print(f"{num_a} // {num_b} = {division}")
+
+print("--------------------------------------")
+
+# 4. Powerful operations
+
+multiply_numbers = num_a * num_b
+power = num_a ** num_b
+reminder = num_a % num_b
+
+print(multiply_numbers)
+print(power)
+print(reminder)
+
+print("--------------------------------------")
+
+# 5. Find average
+
+average = (num_a + num_b) / 2
+print(average)
+
+print("--------------------------------------")
+
+# 6. Area of a circle
+
+radius = 5
+
+circle_area = 2 * math.pi * radius
+
+circle_area_final = round(circle_area, 2)
+
+print(circle_area_final)
+
+print("--------------------------------------")
+
+# 7. Area of an equilateral triangle
+
+side_length = 10
+
+
+
+
+
+
+
+
+
