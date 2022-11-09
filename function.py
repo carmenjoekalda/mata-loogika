873f1697dd66104562035da228128886663c566e
cjoekalda <cjoekalda@gmail.com>
2022-11-09 12:56:06 +0200

    Function's basics

---
+++ function.py
@@ -0,0 +1,30 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    '''Print I'm inside the function.'''
+    print("I´m inside the function")
+
+
+# my_name_is(name)
+def my_name_is(name):
+    '''Print My name is (name).'''
+    print("My name is " + name)
+
+
+# sum_six(num)`
+def sum_six(num):
+    '''Return number + 6.'''
+    return int(num) + 6
+
+# sum_numbers()
+def sum_numbers(a, b):
+    """Return the sum of numbers. """
+    return a + b
+
+
+# usd_to_eur()
+def usd_to_eur(usd):
+    """Return USD in euros."""
+    return usd * 0.8
