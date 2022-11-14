71d2f423c766e81e3e8a49bcd4d2a642c986820f
cjoekalda <cjoekalda@gmail.com>
2022-11-14 13:41:13 +0200

    Strings

---
+++ Strings.py
@@ -0,0 +1,27 @@
+# 1. Declaring first_name & last_name
+first_name = "James"
+last_name = "Bond"
+
+
+# 2. Full name and seld description sentence
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+self_description_sentence = "My name is " + last_name + ", " + first_name + " " + last_name + "."
+print(self_description_sentence)
+
+
+# 3. cake string
+cake = "vahukoor\n" \
+       "marjad\n" \
+       "täidis\n" \
+       "põhi"
+print(cake)
+
+
+# 4. Slice notation
+original_string = "Programming is fun!"
+backwards = original_string [::-1]
+every_other = original_string [:: 2]
+first_word_reversed = original_string [10::-1]
