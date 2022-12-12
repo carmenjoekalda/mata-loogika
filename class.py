ba23938b1b658c8bd0f7f3a4c3ed153593e49794
cjoekalda <cjoekalda@gmail.com>
2022-12-12 15:19:03 +0200

    class

---
+++ class.py
@@ -0,0 +1,7 @@
+"""New class named Student."""
+class Student():
+    """Student has constructors name and finished."""
+    def __init__(self, name, finished = False):
+        """Has constructors name and finish"""
+        self.name = name
+        self.finished = finished
